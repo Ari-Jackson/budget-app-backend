87466ffa2b8461ca8fd69ef168e8f83fcceaa909
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-17 08:15:48 -0400

    Adds put and delete endpoints for /:id route

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -12,17 +12,45 @@ transactionRouter
     res.status(201).json(transactionModel[transactionModel.length - 1]);
   });
 
-transactionRouter.route("/:id").get((req, res) => {
-  const { id } = req.params;
-  const matchingTransaction = transactionModel.find((trans) => {
-    return trans.id === Number(id);
-  });
+const findsTransaction = (id) => (trans) => {
+  return trans.id === Number(id);
+};
 
-  if (!matchingTransaction) {
-    res.status(404).send("No transaction found");
-  } else {
-    res.status(200).json(matchingTransaction);
-  }
-});
+transactionRouter
+  .route("/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const matchingTransaction = transactionModel.find(findsTransaction(id));
+
+    if (!matchingTransaction) {
+      res.status(404).send("No transaction found");
+    } else {
+      res.status(200).json(matchingTransaction);
+    }
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    const matchingTransaction = transactionModel.find(findsTransaction(id));
+    const matchingIndex = transactionModel.findIndex(findsTransaction(id));
+
+    if (!matchingTransaction) {
+      res.status(404).send("No matching transaction found");
+    } else {
+      transactionModel[matchingIndex] = req.body;
+      res.status(200).json(transactionModel[matchingIndex]);
+    }
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const matchingTransaction = transactionModel.find(findsTransaction(id));
+    const matchingIndex = transactionModel.findIndex(findsTransaction(id));
+
+    if (!matchingTransaction) {
+      res.status(404).send("No matching transaction found");
+    } else {
+      transactionModel.splice(matchingIndex, 1);
+      res.status(200).json(transactionModel);
+    }
+  });
 
 module.exports = transactionRouter;
